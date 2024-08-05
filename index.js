ffaa98600fc66ea7574ae0a1f3e74b0da820fc98
leduckhuong <leduckhuong@kali.kali.org>
2024-08-05 09:02:35 +0700

    cors

--- index.js
+++ index.js
@@ -25,13 +25,7 @@ db.connect();
 
 var whitelist = ['https://chat-q4eo.onrender.com', 'http://example2.com']
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
+    origin: whitelist
 }
 
 app.use(cors(corsOptions));
